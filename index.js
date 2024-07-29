29469ff9ec9e4e7c4bf188892a4333f5d3b54f61
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-07-29 07:24:47 +0100

    Final commit of changes before review

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { employees } from "./employees.js";
+import { employees } from "./employees-optional.js";
 
-const employeeListEl = document.getElementById("employee-list");
 const roleSearchEl = document.getElementById("roles");
 const nameSearchEl = document.getElementById("search-name");
-const roles = [];
+
+let filteredEmployees = [...employees];
 
 roleSearchEl.addEventListener("change", (e) => {
     renderEmployees(e.target.value, nameSearchEl.value);
@@ -15,48 +15,123 @@ nameSearchEl.addEventListener("input", (e) => {
 
 function renderEmployees(team, name = "") {
 
+    const employeeListEl = document.getElementById("employee-list");
+
+    /* Could just use employees in filterEmployeesByTeam but I wanted to write it
+       so that both functions are independent of eath other */
+    filteredEmployees = [...employees];
+
+    filteredEmployees = filterEmployeesByTeam(team);
+
+    filteredEmployees = filterEmployeesByName(name);
+
     employeeListEl.innerHTML = '';
-    employees.filter(employee => {
-
-        if ((team === "everyone" || employee.team === team) && (name === "" || employee.name.includes(name))) {
-          employeeListEl.innerHTML += `
-          <div class="employee-card">
-              <img src="./images/photos/${employee.image}" class="avatar" alt="Avatar of employee ${employee.name}">
-              <h2>${employee.name}</h2>
-              <h3>${employee.title}</h3>
-              ${employee.bio}
-              <div class="socials-container">
-                  ${employee.social.twitter ?
-                    `<span class="social"><a href="${employee.social.twitter}"><i class="fa-brands fa-twitter"></i></a></span>`
-                    : ''
-                  }
-                  ${employee.social.linkedin ?
-                    `<span class="social"><a href="${employee.social.linkedin}"><i class="fa-brands fa-linkedin"></i></a></span>`
-                    : ''
-                  }
-              </div>
-          </div>
-          `;
-        }
-
-        if (!roles.includes(employee.team)) {
-            roles.push(employee.team);
-        };
-    })
+    filteredEmployees.forEach(member => {
+            
+        employeeListEl.innerHTML += `
+            <div class="employee-card">
+                <img src="./images/photos/${member.image}" class="avatar" alt="Avatar of employee ${member.name}">
+                <h2>${member.name}</h2>
+                <h3>${member.title}</h3>
+                ${member.bio}
+                <div class="socials-container">
+                    ${member.social.twitter ?
+                      `<span class="social"><a href="${member.social.twitter}"><i class="fa-brands fa-twitter"></i></a></span>`
+                      : ''
+                    }
+                    ${member.social.linkedin ?
+                      `<span class="social"><a href="${member.social.linkedin}"><i class="fa-brands fa-linkedin"></i></a></span>`
+                      : ''
+                    }
+                </div>
+            </div>
+            `;
+    });
 
 };
+
+function filterEmployeesByTeam(team = "everyone") {
+    return filteredEmployees.reduce((members, currentMember) => 
+        team === "everyone" || currentMember.team === team
+            ? members.concat(currentMember.members)
+            : members
+    ,[]);
+}
+
+function filterEmployeesByName(name) {
+  return filteredEmployees.reduce((members, currentMember) =>
+      name === "" || currentMember.name.toLowerCase().includes(name.toLowerCase()) /* Make search case insensitive */
+            ? members.concat(currentMember)
+            : members
+  ,[]);
+}
+
+function initialisePage() {
+
+    renderRoles();
+
+    renderEmployees("everyone");
+}
 
 function renderRoles() {
     
     roleSearchEl.innerHTML = `<option value="everyone">Everyone</option>`;
-    roles.forEach(role => {
+    employees.forEach(team => {
         roleSearchEl.innerHTML += `
-        <option value="${role}">${role}</option>
+        <option value="${team.team}">${team.team}</option>
         `;
     });
 
 };
 
-renderEmployees("everyone");
+initialisePage();
 
-renderRoles();
+// function renderEmployees(team, name = "") {
+
+//     employeeListEl.innerHTML = '';
+//     employees.forEach(employee => {
+            
+//         if (team === "everyone" || employee.team === team) {
+
+//             employee.members.forEach(member => {
+
+//                 if (name === "" || member.name.toLowerCase().includes(name.toLowerCase())) {
+//                     employeeListEl.innerHTML += `
+//                     <div class="employee-card">
+//                         <img src="./images/photos/${member.image}" class="avatar" alt="Avatar of employee ${member.name}">
+//                         <h2>${member.name}</h2>
+//                         <h3>${member.title}</h3>
+//                         ${member.bio}
+//                         <div class="socials-container">
+//                             ${member.social.twitter ?
+//                               `<span class="social"><a href="${member.social.twitter}"><i class="fa-brands fa-twitter"></i></a></span>`
+//                               : ''
+//                             }
+//                             ${member.social.linkedin ?
+//                               `<span class="social"><a href="${member.social.linkedin}"><i class="fa-brands fa-linkedin"></i></a></span>`
+//                               : ''
+//                             }
+//                         </div>
+//                     </div>
+//                     `;
+//                 };
+//             });
+//         };
+
+//         if (!roles.includes(employee.team)) {
+//             roles.push(employee.team);
+//         };
+//     });
+
+// };
+
+// function renderRoles() {
+    
+//     roleSearchEl.innerHTML = `<option value="everyone">Everyone</option>`;
+//     roles.forEach(role => {
+//         roleSearchEl.innerHTML += `
+//         <option value="${role}">${role.charAt(0).toUpperCase() + role.slice(1)}</option>
+//         `;
+//     });
+
+// };
